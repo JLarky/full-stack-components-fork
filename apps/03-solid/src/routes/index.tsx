722be01d8134d7f990fbea87071d5cd084023d2f
JLarky <jlarky@gmail.com>
2022-12-08 11:30:45 -0700

    full stack components
    - CustomerCombobox gets its own endpoint automatically _m/src/components/customers.tsx/0/customers0
    - type safety doesn't require us to cast anything
      - but you can still get in trouble by returning empty string (turns
        into undefined) or returning class instances (turns into a string)
    - it doesn't preserve value from previous server call automatically
    - HMR and error handling in solid some times fails silently
    - https://youtu.be/6S6hIisCcws

--- apps/03-solid/src/routes/index.tsx
+++ apps/03-solid/src/routes/index.tsx
@@ -1,31 +1,29 @@
-import { A } from "solid-start";
-import Counter from "~/components/Counter";
+import { A } from 'solid-start'
+import { CustomerCombobox } from '~/components/customers'
+
+export const submitButtonClasses =
+	'w-full rounded bg-green-500 py-2 px-4 text-white hover:bg-green-600 focus:bg-green-400'
 
 export default function Home() {
-  return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
-    </main>
-  );
+	return (
+		<main class="mx-auto p-4 text-center text-gray-700">
+			<h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">
+				New invoice
+			</h1>
+			<form method="post" class="flex flex-col gap-4">
+				{/* <CustomerCombobox error={actionData?.errors.customerId} /> */}
+				<CustomerCombobox />
+				<div>
+					<button
+						type="submit"
+						name="intent"
+						value="create"
+						class={submitButtonClasses}
+					>
+						Create Invoice
+					</button>
+				</div>
+			</form>
+		</main>
+	)
 }
